7ade299b8e4ca2dcca37b26d18ccdf5e86abc396
FIA CLEAN & CARE <admin@fiacleancare.com>
2026-09-06 23:39:59 +0530

    Fix offline-online bidirectional sync: prevent cloud overwriting offline changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'fia-clean-care-v2';
+﻿const CACHE_NAME = 'fia-clean-care-v3';
 
 // Static core assets to pre-cache immediately on service worker install
 const PRECACHE_ASSETS = [
